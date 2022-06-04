9de71ba20b8c62f913e2f8f160ba6811dcc0ef83
futurezzz <79406446+futurezzz@users.noreply.github.com>
2022-06-04 23:15:30 +0900

    Add files via upload

--- js/quiz.js
+++ js/quiz.js
@@ -403,5 +403,5 @@ mic.addEventListener("click", () => {
 
 //--------------사전 열어보기-------------------------------------//
 dictionary.addEventListener('click',()=>{
-  window.open("../dictionary.html", "Dictionary", "toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=700, height=700, left=0, top=0" );
+  window.open("dictionary.html", "Dictionary", "toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=700, height=700, left=0, top=0" );
 })
